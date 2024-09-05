f7dda27a9ed72f0defde6ec5f78bde3e207da633
Zeno van Ditzhuijzen <z.c.vanditzhuijzen@students.uu.nl>
2024-09-05 09:12:37 +0200

    Extra's: SCSS
    Added SCSS support and updated the index page to use SCSS.

--- mynuxtproject/pages/index.vue
+++ mynuxtproject/pages/index.vue
@@ -2,15 +2,17 @@
   <div>
     <h1>Index</h1>
     <p>Paragraph of text on the index page.</p>
-	<p> <nuxt-link to="/mypage">Go to My Page</nuxt-link> </p>
-	<p> <nuxt-link to="/count">Go to Count</nuxt-link> </p>
+    <p><nuxt-link to="/mypage">Go to My Page</nuxt-link></p>
+    <p><nuxt-link to="/count">Go to Count</nuxt-link></p>
     <p><nuxt-link to="/message/add">Go to Add Message</nuxt-link></p>
     <p><nuxt-link to="/message/read">Go to Read Messages</nuxt-link></p>
-	<p> <nuxt-link to="/testapipage">Go to Test API Page</nuxt-link> </p>
+    <p><nuxt-link to="/testapipage">Go to Test API Page</nuxt-link></p>
   </div>
 </template>
 
 <script>
+import './index.scss';
+
 export default {
   name: 'IndexPage'
 };
